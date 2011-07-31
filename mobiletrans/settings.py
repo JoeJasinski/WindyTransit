bb3b38be8d5fed90d3af283f0706e605958df4e6
Joe Jasinski <joe.jasinski@gmail.com>
2011-07-31 12:25:19 -0500

    Basic project structure and setup

--- mobiletrans/settings.py
+++ mobiletrans/settings.py
@@ -1,24 +1,34 @@
 # Django settings for mobiletrans project.
+import os
+from mtsettings.local_settings import (VENV_ROOT, PROJECT_ROOT )
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+#######################
+#####  These settings should go in local_settings.py
+####################
+#DEBUG = True
+#TEMPLATE_DEBUG = DEBUG
+#
+#ADMINS = (
+#    # ('Your Name', 'your_email@example.com'),
+#)
+#
+#MANAGERS = ADMINS
+#
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#        'NAME': 'mobiletrans',
+#        'USER': 'mobiletrans_user',
+#        'PASSWORD': '1234',
+#        'HOST': 'localhost',
+#        'PORT': '',
+#    }
+#}
 
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
+#######################
+#####  END These settings should go in local_settings.py
+####################
 
-MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/jjasinsk/Sites/env/mobiltrans/mobiletrans/sqlite.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -45,18 +55,20 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(VENV_ROOT, 'htdocs','media').replace('\\','/') + '/'
+
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(VENV_ROOT,'htdocs','static')
+
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -72,6 +84,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'staticfiles'),
 )
 
 # List of finder classes that know how to find static files in
@@ -106,6 +119,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'templates').replace('\\','/'),
+
 )
 
 INSTALLED_APPS = (
@@ -115,10 +130,11 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'django.contrib.gis',
+    'mtcore',
+    'mtlocation',
+
 )
 
 # A sample logging configuration. The only tangible logging
@@ -143,3 +159,6 @@ LOGGING = {
         },
     }
 }
+
+
+from mtsettings.local_settings import *
