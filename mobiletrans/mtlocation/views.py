422facb682545d796f54b33bb8a95beeb5b788b9
Joe Jasinski <joe.jasinski@gmail.com>
2011-08-22 03:07:22 -0500

    Added neighborhood support

--- mobiletrans/mtlocation/views.py
+++ mobiletrans/mtlocation/views.py
@@ -16,9 +16,13 @@ def renderkml(request, lat=None, long=None):
 
     template = loader.get_template('mtlocation/locale.kml')
     
+    try:
+        neighborhood = models.Neighborhood.sub_objects.filter(area__contains=params.ref_pnt)[0]
+    except IndexError:
+        neighborhood = ""
     placemarks = models.Location.objects.filter(point__distance_lte=(params.ref_pnt, D(**params.d) )).distance(params.ref_pnt).order_by('distance') 
-    c = Context({ 'placemarks': placemarks[:params.limit], 'site': Site.objects.get_current(), 'STATIC_URL':settings.STATIC_URL, })
-    
+    c = Context({ 'neighborhood':neighborhood, 'placemarks': placemarks[:params.limit], 'site': Site.objects.get_current(), 'STATIC_URL':settings.STATIC_URL, })
+
     response = HttpResponse(template.render(c), content_type="application/vnd.google-earth.kml+xml")
     response['Content-Disposition'] = 'attachment; filename=locale.kml'
     return response 
