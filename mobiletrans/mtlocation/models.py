4383df186ea5d8355828a3237c9df4378c958fc0
Joe Jasinski <joe.jasinski@gmail.com>
2013-02-28 04:11:24 -0600

    fix for border import

--- mobiletrans/mtlocation/models.py
+++ mobiletrans/mtlocation/models.py
@@ -456,10 +456,10 @@ cta_raillines2_mapping = {
 
 class CityBorder(models.Model):
     objectid = models.IntegerField()
-    name = models.CharField(max_length=25)
-    shape_area = models.FloatField()
-    shape_len = models.FloatField()
-    geom = models.PolygonField(srid=4326)
+    name = models.CharField(max_length=25, blank=True, null=True)
+    shape_area = models.FloatField(blank=True, null=True)
+    shape_len = models.FloatField(blank=True, null=True)
+    area = models.MultiPolygonField(srid=4326)
     
     objects = models.GeoManager()
     
