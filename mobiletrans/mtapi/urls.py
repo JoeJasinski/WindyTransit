b9d9297dcf52f9f4de3b3a889b53737920201066
Joe Jasinski <joe.jasinski@gmail.com>
2014-06-09 00:24:59 -0500

    remove formatter

--- mobiletrans/mtapi/urls.py
+++ mobiletrans/mtapi/urls.py
@@ -8,13 +8,13 @@ from mobiletrans.mtapi import views
 urlpatterns_2 = patterns('',
     url(r'^transitroutes(\.(?P<emitter_format>.+))?$', 
         views.ListTransitRoutesView.as_view(), name='api2_transitroutes'),
-    url(r'^transitroutes/(?P<route_id>\w+)/transitroute(\.(?P<emitter_format>.+))?$', 
+    url(r'^transitroutes/(?P<route_id>\w+)/transitroute$', 
         views.ListTransitRouteView.as_view(), { }, name="api2_transitroute"),
-    url(r'^transitroutes/(?P<uuid>[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})/transitroute(\.(?P<emitter_format>.+))?$', 
+    url(r'^transitroutes/(?P<uuid>[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12})/transitroute$', 
         views.ListTransitRouteView.as_view(), { }, name="api2_transitroute"),
-    url(r'^locations(\.(?P<emitter_format>.+))?$', 
+    url(r'^locations$', 
         views.LocationDataView.as_view() , { }, name="api_locations"),  
-    url(r'^transitstops(\.(?P<emitter_format>.+))?$', 
+    url(r'^transitstops$', 
         views.TransitStopDataView.as_view(), { }, name="api2_transitstops"), 
 )
 
